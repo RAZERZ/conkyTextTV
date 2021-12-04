d7b4065b245fd5eff82388accdf899ec1d1bf4d5
RAZERZ <ramiabouzahra@gmail>
2021-12-04 23:45:34 +0100

    Removed personal color preference

--- fetcher.sh
+++ fetcher.sh
@@ -1,5 +1,5 @@
 curl https://www.svt.se/text-tv/100 > ~/Documents/i3/conkyTextTV/data.txt &&
   python ~/Documents/i3/conkyTextTV/parser.py &&
-  mogrify -format png -fill "#000000" -opaque "#0000FF" ~/Documents/i3/conkyTextTV/100.png &&
-  mogrify -format png -fill "#FF00FF" -opaque "#FFFFFF" ~/Documents/i3/conkyTextTV/100.png &&
+  #mogrify -format png -fill "#000000" -opaque "#0000FF" ~/Documents/i3/conkyTextTV/100.png &&
+  #mogrify -format png -fill "#FF00FF" -opaque "#FFFFFF" ~/Documents/i3/conkyTextTV/100.png &&
   convert ~/Documents/i3/conkyTextTV/100.png -transparent black ~/Documents/i3/conkyTextTV/100_transparent.png
